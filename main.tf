21ea4e24bb86e6c57a574879e42d032c328ad787
chadclaussen <chadclaussen@gmail.com>
2020-08-26 17:47:31 +0000

    Updated my code.

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  Department          = "true"
+  Billable            = "devops"
 }
 
 resource "azurerm_subnet" "subnet" {
